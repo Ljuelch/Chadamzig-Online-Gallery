9f167faf3b52e9d3fdcc0923b33ac20ba6a1f675
Ljuelch <leojuelch@yahoo.de>
2024-09-25 16:13:38 +0200

    commissions component init

---
+++ src/app/commissions/commissions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-commissions',
+  templateUrl: './commissions.component.html',
+  styleUrl: './commissions.component.scss'
+})
+export class CommissionsComponent {
+
+}
